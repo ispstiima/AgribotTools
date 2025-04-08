9991d1afa4a78d198d9c6f572020e6aefc6b326c
Michele Paradiso <michelepara17@gmail.com>
2025-04-08 14:30:48 +0200

    Refactor converter functions to enhance logging and file copying; integrate tqdm for progress indication (#19)

--- converter/utils.py
+++ converter/utils.py
@@ -8,8 +8,8 @@ log = logging.getLogger("Utils")
 logging.basicConfig(format='%(asctime)s - %(name)s [%(levelname)s] | %(message)s', level=logging.INFO)
 
 
-def copy_files_monitored(source_path: Path, dest_path: Path, dirs_exist_ok: bool = True) -> bool:
-    log.info(f"Copying folder <{source_path.name}> to <{dest_path.name}>")
+def copy_files_monitored(source_path: Path, dest_path: Path, dirs_exist_ok: bool = True, desc="Copying files") -> bool:
+    log.info(f"Copying folder <{source_path}> to <{dest_path}>")
 
     if not source_path.exists():
         log.error("Source path for copy tree does not exist.")
@@ -25,7 +25,7 @@ def copy_files_monitored(source_path: Path, dest_path: Path, dirs_exist_ok: bool
 
     num_files = sum(1 for file in source_path.iterdir() if file.is_file())
 
-    for source_file_path in tqdm(source_path.iterdir(), total=num_files, ascii="░▒█", desc="Copying files"):
+    for source_file_path in tqdm(source_path.iterdir(), total=num_files, ascii="░▒█", desc=desc):
         if source_file_path.is_dir():
             log.warning("Directory found. Skipping it.")
             continue
