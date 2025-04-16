edb07d0517195d778204bfa7d09655b52fd58722
Giuseppe Veronico <80847145+gveronico98@users.noreply.github.com>
2025-04-16 12:40:47 +0200

    Enhanced YOLO to Label Studio Converter (#21)
    * Started working on yolo_to_ls
    
    * Refined input and output logic of yolo_to_ls()
    
    * Documented each function related to yolo_to_ls()

--- examples/yolo_to_ls_test.py
+++ examples/yolo_to_ls_test.py
@@ -1,12 +1,21 @@
 from converter.converter import yolo_to_ls
 
 print("=============== yolo_to_ls test ===============")
+
 print("\n\n>>> Converting yolo bbox to ls format...")
-yolo_to_ls("../yolo_temp/canon_bbox", "ls_canon_temp_bbox", "bbox")
+ls_bbox_dir = yolo_to_ls("bbox", "../out/canon_yolo_temp/canon_bbox")
+print(f"Output path: {ls_bbox_dir}")
+
 print("\n\n>>> Converting yolo seg to ls format...")
-yolo_to_ls("../yolo_temp/canon_seg", "ls_canon_temp_seg", "seg")
+ls_seg_dir = yolo_to_ls("seg","../out/canon_yolo_temp/canon_seg")
+print(f"Output path: {ls_seg_dir}")
+
 print("\n\n>>> Converting ls bbox to yolo format...")
-yolo_to_ls("../yolo_temp/canon_bbox_new", "ls_canon_temp_bbox", "bbox", reverse=True)
+yolo_bbox_dir = yolo_to_ls("bbox", ls_base_name="canon_bbox_ls", reverse=True)
+print(f"Output path: {yolo_bbox_dir}")
+
 print("\n\n>>> Converting ls seg to yolo format...")
-yolo_to_ls("../yolo_temp/canon_seg_new", "ls_canon_temp_seg", "seg", reverse=True)
+yolo_seg_dir = yolo_to_ls("seg", ls_base_name="canon_seg_ls", reverse=True)
+print(f"Output path: {yolo_seg_dir}")
+
 print("\n\n>>> Completed")
